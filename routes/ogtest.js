b3274d70c15c63c9dea82dc83ace09cc1c31d3d3
vitkrv <vitkrv@gmail.com>
2017-08-31 21:57:11 +0300

    new headers

--- routes/ogtest.js
+++ routes/ogtest.js
@@ -10,11 +10,10 @@ router.get('/', function (req, res) {
   var options = {
     url: custom_url || 'http://ogp.me/',
     headers: {
-      'accept-language': 'en',
-      'accept': 'text/html;',
+      'accept': 'text/html',
+      'accept-encoding': "gzip, deflate, sdch, br",
       'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
-    },
-    encoding: null
+    }
   };
 
   request(options, function (err, response) {
