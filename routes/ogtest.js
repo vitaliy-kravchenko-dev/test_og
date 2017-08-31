442619460087ebe41e17192fb3b2a58af52db7f5
vitkrv <vitkrv@gmail.com>
2017-08-31 21:58:48 +0300

    new user agent

--- routes/ogtest.js
+++ routes/ogtest.js
@@ -12,7 +12,7 @@ router.get('/', function (req, res) {
     headers: {
       'accept': 'text/html',
       'accept-encoding': "gzip, deflate, sdch, br",
-      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
+      'user-agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3'
     }
   };
 
